d2ece83457bc72bde0a669d60eb94f893b9b889d
Manu Morante <manu@estadologico.com>
2022-12-03 03:19:59 +0100

    assetPrefix and ENV prod check

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-const nextConfig = {
-  basePath: '/angelredouan',
-}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '/angelredouan/' : '',
+  basePath: isProd ? '/angelredouan/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
